dfa057b5ecaa1bdd2f6b7d321cb13e191124c24b
Brittany Nicole <bnsquires1@gmail.com>
2024-06-10 09:51:29 -0400

    updates

--- routes/posts.js
+++ routes/posts.js
@@ -1,2 +1,14 @@
-// //update a bweet by ID
-// router.patch('/:id', usersCtrl.chgUser);
+const { Router } = require('express');
+const postsCtrl = require('../controllers/posts.js');
+
+const router = Router();
+
+//Create a post
+router.post('/', postsCtrl.createPost);
+
+//delete a user by ID
+router.delete('/:id', postsCtrl.deletedPost);
+
+//update a bweet by ID
+router.patch('/:id', postsCtrl.editPost);
+
